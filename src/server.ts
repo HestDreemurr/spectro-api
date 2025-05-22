a754b84c8fe7bc676cf49850f39a17ebc0805262
Hest <hestdatabase@gmail.com>
2025-05-21 23:04:42 -0300

    Commit Inicial

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app.ts"
+
+app.listen(3333, () => {
+  console.log("[+] A API está rodando. ")
+})
---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,19 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository"
+import { ICreateUserRequestDTO } from "./CreateUserDTO"
+import User from "../../entities/User"
+
+export class CreateUserUseCase {
+  constructor(
+    private usersRepository: IUsersRepository
+  ) {}
+  
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
+    
+    if (userAlreadyExists) {
+      throw new Error("O usuário já existe.")
+    }
+    
+    await this.usersRepository.save(data)
+  }
+}
